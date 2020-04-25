9d5a0c739eb8114c4813f60158d16cdc2f53bce8
Core Three Designs <CoreThreeDesign@gmail.com>
2020-04-25 15:55:17 -0500

    :sparkles: Babel Presets for Core Three Dev Team

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-babel-preset-core-three
+# babel-preset-Core3
+
+> Babel presets for Core3 Designs
+
+## Usage
+- For more information please see [documentation](https://babeljs.io/docs/en/presets)
+- To check out our website please see [Core three Designs](https://corethreedesign.com/)
+
+## Install
+
+Using npm:
+
+```sh
+npm install --save-dev babel-preset-core3
+```
+
+or using yarn:
+
+```sh
+yarn add babel-preset-core3 --dev
+```
+
+
+`babel.config.js`
+```javascript
+const development =
+    process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'qa';
+
+module.exports = {
+  presets: [
+    ['babel-preset-core3',
+      { development },
+    ]],
+};
+```
---
+++ src/index.js
@@ -0,0 +1,35 @@
+import { declare } from "@babel/helper-plugin-utils";
+import presetEnv from "@babel/preset-env";
+import presetReact from "@babel/preset-react";
+import classProperties from "@babel/plugin-proposal-class-properties";
+import privateMethods from "@babel/plugin-proposal-private-methods";
+
+export default declare((api, opts) => {
+  api.assertVersion(7);
+  const { development } = opts; // If Development mode.
+
+  const presetENVOptions = {
+    useBuiltIns: 'usage',
+    corejs: {
+      version: 3,
+    },
+    shippedProposals: true,
+  };
+
+  if(development) {
+    presetENVOptions.targets = {
+      chrome: '80'
+    }
+  }
+
+  return {
+    presets: [
+      [presetEnv, presetENVOptions],
+      presetReact,
+    ],
+    plugins: [
+      classProperties,
+      privateMethods,
+    ],
+  };
+});
